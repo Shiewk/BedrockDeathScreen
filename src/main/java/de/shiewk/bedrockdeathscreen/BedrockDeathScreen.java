e17b28ef353d01f94f3d055a784e8bcbf975a325
Shiewk <shiewkmail@shiewk.de>
2024-06-14 16:26:10 +0200

    Initial commit

---
+++ src/main/java/de/shiewk/bedrockdeathscreen/BedrockDeathScreen.java
@@ -0,0 +1,10 @@
+package de.shiewk.bedrockdeathscreen;
+
+import net.fabricmc.api.ModInitializer;
+
+public class BedrockDeathScreen implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/de/shiewk/bedrockdeathscreen/client/BedrockDeathScreenClient.java
@@ -0,0 +1,10 @@
+package de.shiewk.bedrockdeathscreen.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BedrockDeathScreenClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
